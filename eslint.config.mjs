6185a26b4a2da611d816b99aacda994808f2e563
KMTonmoy <tonmoyahamed2009@gmail.com>
2025-01-15 11:18:35 +0600

    Some Solved

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,18 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    rules: {
+      // Allow `window` object to be used in components
+      "no-undef": ["error", { typeof: true }],
+      // Ignore usage of `window` in server-side code
+      "no-use-before-define": ["error", { functions: false, classes: true }],
+      // Skip any `window` or `document` references in server-side code
+      "no-restricted-globals": ["error", "window", "document"],
+    },
+  },
+];
 
 export default eslintConfig;
